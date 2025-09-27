9d16bfc109a294df2047bd8ed0790b854229f74d
himan07 <himan9714@gmail.com>
2025-09-27 12:15:18 +0530

    changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -160,27 +160,24 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      "I didn’t think it was possible to streamline our panels so efficiently, but Himanshu proved me wrong.",
+    name: "Rahul Bhatnagar",
+    designation: "Senior Vice President",
+    company: "",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+      "I've never met a web developer who truly cares about their clients' success like Himanshu does.",
+    name: "Defence Of India",
+    designation: "",
+    company: "",
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+      "After Himanshu optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Market Xcel",
+    designation: "",
+    company: "",
   },
 ];
 
